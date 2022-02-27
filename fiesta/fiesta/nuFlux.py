correct units in atmospheric flux

fiesta/fiesta/nuFlux.py
@@ -23,16 +23,17 @@ def read_flux_file(f):
     d["nu_mu_bar"]=np.array(numubar)
     """
     d={}
+    units_factor = 1.e-4/(4*np.pi) #from m^-2 to cm^-2, and from full solid angle to sr^-1
     log_e, nue, numu, nuebar, numubar = ([] for i in range (5))
     File = open(f,"r")
     lines = File.readlines()
     for line in lines:
         columns = line.split(' ')
         log_e.append(float(columns[0]))
-        nue.append(float(columns[1]))
-        numu.append(float(columns[2]))
-        nuebar.append(float(columns[3]))
-        numubar.append(float(columns[4]))
+        nue.append(float(columns[1])*units_factor)
+        numu.append(float(columns[2])*units_factor)
+        nuebar.append(float(columns[3])*units_factor)
+        numubar.append(float(columns[4])*units_factor)
     d["log_E"]=np.array(log_e)
     d["E"]=np.power(10, np.array(log_e))
     d["nu_e"]=np.array(nue)