fde2e6e20996342a76155e1b355a514d878c017c
rodri <rgracia@km3net.de>
2022-03-22 09:39:23 +0100

    clean fitXs.py

--- scripts/fitXs.py
+++ scripts/fitXs.py
@@ -1,9 +1,8 @@
 """
-Usage: fitAtmFlux.py [-o OUTPUT_FILE] -i INPUT_FILE -c CHANNEL -t JSON_TABLE
+Usage: fitXs.py -i INPUT_FILE -c CHANNEL -t JSON_TABLE
 
 Options:
   -h --help                      Help.
-  -o --output_file OUTPUT_FILE   Output file.
   -i --input_file INPUT_FILE     Input file.
   -c --channel CHANNEL           Interaction channel (nu_cc, nu_nc, anu_cc, anu_nc).
   -t --json_table JSON_TABLE     JSON formatted table with all the analysis parameters.
@@ -22,9 +21,6 @@ import json
 def main():
     arguments = docopt(__doc__)
 
-    if (arguments['--output_file']==None):
-        arguments['--output_file']=Path(arguments['--input_file']).stem+'.json'
-    
     xs=nx.read_xs_file(arguments["--input_file"])
 
     logEmin=5
@@ -35,40 +31,8 @@ def main():
 
     a,b = curve_fit(nx.dis_xs, xs["log_E"][i1:i2], xs[arguments["--channel"]][i1:i2], maxfev=2000)
     
-    # if(Path(arguments['--output_file']).is_file()):
-    #     print("output file exists. Updating it")
-    #     with open(arguments["--output_file"], 'r') as json_file:
-    #         d = json.load(json_file)
-    #     json_file.close()
-
-    #     d[arguments["--channel"]]["a1"] = a[0]
-    #     d[arguments["--channel"]]["b1"] = a[1]
-    #     d[arguments["--channel"]]["a2"] = a[2]
-    #     d[arguments["--channel"]]["b2"] = a[3]
-        
-    #     with open(arguments["--output_file"], 'w') as outfile:
-    #         json.dump(d, outfile)
-    #     outfile.close()
-        
-    # else:
-    #     print("output file does not exist. Creating it")
-    #     d={}
-    #     for key in ["cc_nu", "nc_nu", "cc_nu_bar", "nc_nu_bar"]:
-    #         d[key]={"a1":0,"b1":0,"a2":0,"b2":0}
-            
-    #     d[arguments["--channel"]]["a1"] = a[0]
-    #     d[arguments["--channel"]]["b1"] = a[1]
-    #     d[arguments["--channel"]]["a2"] = a[2]
-    #     d[arguments["--channel"]]["b2"] = a[3]
-        
-    #     with open(arguments["--output_file"], 'w') as outfile:
-    #         json.dump(d, outfile)
-    #     outfile.close()
-
     t = tbl.table.from_json(arguments["--json_table"])
-    
     t.set_xs(arguments["--channel"], logEmin, logEmax, a[0], a[1], a[2], a[3])
-
     t.write(arguments["--json_table"])
         
 if __name__== '__main__':
